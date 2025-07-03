5996398ee3c7eb2c707670b4a3067af142cbf8b2
Hiram Pina <hiram.p@gmail.com>
2025-07-03 15:28:14 -0600

    Reformat setup.sh for more general cases and make it verbose

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
-#/bin/bash
+#!/bin/bash
 
-cp .vimrc ~/.vimrc
+# Define source and destination
+SRC_FILE=".vimrc"
+DEST="$HOME/.vimrc"
+
+# Copy the configuration file
+cp "$SRC_FILE" "$DEST"
+
+# Output result
+echo "✅ .vimrc has been successfully copied to $DEST"
+
